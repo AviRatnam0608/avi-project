35610d96b2e8d9cb11f8432af859d08bc789598c
Avinash Ratnam <avinashr@umich.edu>
2024-09-14 21:35:15 -0400

    Added a link to db with an updated schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { desc } from "drizzle-orm";
 import Link from "next/link";
 import { db } from "~/server/db";
 
@@ -6,39 +7,20 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"; // This will force the page to be dynamically generated
 
-const allImages = [
-  "https://utfs.io/f/LkYYOTSW20gj5Se405Fhf6wE2eQPagx5YU8jKRirLTB1Asp4",
-  "https://utfs.io/f/LkYYOTSW20gjOaLYTV0wefJyCgm6vDG8ZKPwRnks35VlYt71",
-  "https://utfs.io/f/LkYYOTSW20gjIPXhKZsHNrtomysT05z624R1Ydx9BuDjIMGv",
-  "https://utfs.io/f/LkYYOTSW20gj6vZjTkW5mbalwn0jiEBeAFpW8L9ZDXdqscfk",
-];
-
-const images = allImages.map((image, index) => ({
-  id: index,
-  src: image,
-  alt: `Image ${index + 1}`,
-}));
-
 // running on server
 const HomePage = async () => {
-  const posts = await db.query.posts.findMany(); // Fetch posts from the database
-
-  console.log(posts); // Log the fetched posts
-
+  // Fetch images from the database [images here is the variable]
+  // FindMany by default returns db items from latest to oldest
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id), // orderBy takes a model and a helper obj
+  });
   return (
     <main className="p-5">
       <div className="grid grid-cols-3 gap-4">
-        {posts.map((post, index) => {
-          return (
-            <div key={post.id + "-" + index} className="relative">
-              {post.name}
-            </div>
-          );
-        })}
-
         {[...images].map((image) => (
           <div key={image.id} className="relative">
-            <img src={image.src} alt={image.alt} className="h-auto w-3/4" />
+            <img src={image.url} className="h-auto w-3/4" />
+            <p>{image.name}</p>
             <Link
               href={`/image/${image.id}`}
               className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 transition-opacity duration-300 hover:opacity-100"
